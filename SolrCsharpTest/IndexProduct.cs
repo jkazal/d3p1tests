dd30b7121eaabdfe820e9951dae8dc66aeae05ab
Johann Kazal <Johann.Kazal@diiage.org>
2020-09-02 17:12:55 +0200

    initial

---
+++ SolrCsharpTest/IndexProduct.cs
@@ -0,0 +1,27 @@
+﻿using SolrNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolrCsharpTest
+{
+    class IndexProduct
+    {
+        [SolrField("productId")]
+        public int Id { get; set; }
+        [SolrField("productName")]
+        public string ProductName { get; set; }
+        [SolrField("description")]
+        public string Description { get; set; }
+        [SolrField("brandName")]
+        public string BrandName { get; set; }
+        [SolrField("categoryName")]
+        public string CategoryName { get; set; }
+        [SolrField("colorName")]
+        public string ColorName { get; set; }
+        [SolrField("sizeName")]
+        public string SizeName { get; set; }
+        [SolrField("ImageUrl")]
+        public string imageUrl { get; set; }
+    }
+}
---
+++ SolrCsharpTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using CommonServiceLocator;
+using SolrNet;
+using System;
+using System.Data.SqlClient;
+using System.Text;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SolrCsharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queryWithForJson = "SELECT * FROM VSIndex";
+            var conn = new SqlConnection("Server=mlljksqlserver.database.windows.net;Database=MLLJK_Victoria;Trusted_Connection=False;MultipleActiveResultSets=true;User Id=adminadmin;Password=Victoria21;Integrated Security=False;");
+            var cmd = new SqlCommand(queryWithForJson, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<IndexProduct> products = new List<IndexProduct>();
+            while ( reader.Read() )
+            {
+                IndexProduct ip = new IndexProduct();
+                ip.BrandName = reader["BrandName"].ToString();
+                ip.Id = (int) reader["Id"];
+                ip.ProductName = reader["ProductName"].ToString();
+                ip.CategoryName = reader["CategoryName"].ToString();
+                ip.ColorName = reader["ColorName"].ToString();
+                ip.SizeName = reader["SizeName"].ToString();
+                ip.imageUrl = reader["ImageUrl"].ToString();
+                products.Add(ip);
+                Console.WriteLine("Add item " + ip.ProductName);
+            }
+
+            var o = 1;
+            Startup.Init<IndexProduct>("http://localhost:8983/solr/azure_db_test");
+            var solr = ServiceLocator.Current.GetInstance<ISolrOperations<IndexProduct>>();
+            solr.AddRange(products);
+            solr.Commit();
+            Console.WriteLine("fin");
+        }
+    }
+}
